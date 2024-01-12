1501d3fa61ce1727b60bbb1aee29163427d342d5
nukkua <marco.salazar.2014.com@gmail.com>
2024-01-12 10:29:38 -0400

    without readme

---
+++ useFetch.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+	const [state, setState] = useState({
+		data: null,
+		isLoading: true,
+		hasError: null
+	});
+
+	const getFetch = async () => {
+		try {
+			const resp = await fetch(url);
+			if (!resp.ok) throw new Error('Error fetching data');
+			const data = await resp.json();
+
+			setState({
+				data: data,
+				isLoading: false,
+				hasError: null
+			});
+		} catch (e) {
+			setState({
+				data: null,
+				isLoading: false,
+				hasError: e || 'something went wrong!'
+			});
+		}
+	};
+
+	useEffect(() => {
+		getFetch();
+	}, [url]);
+
+	return {
+		data: state.data,
+		isLoading: state.isLoading,
+		hasError: state.hasError
+	};
+};
